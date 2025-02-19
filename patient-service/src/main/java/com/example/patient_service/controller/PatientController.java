db0c5ed1ce274c33086e293343f1cfeafeac1748
Leonidas7777777 <lekko7777777@gmail.com>
2025-02-19 15:01:25 +0100

    Eureka server

---
+++ patient-service/src/main/java/com/example/patient_service/controller/PatientController.java
@@ -0,0 +1,36 @@
+package com.example.patient_service.controller;
+
+import com.example.patient_service.model.Patient;
+import com.example.patient_service.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Patient> getPatientById(@PathVariable Long id) {
+        return patientService.getPatientById(id);
+    }
+
+    @PostMapping
+    public Patient addPatient(@RequestBody Patient patient) {
+        return patientService.addPatient(patient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable Long id) {
+        patientService.deletePatient(id);
+    }
+}
